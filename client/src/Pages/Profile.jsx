187b424ba928a50929c67f3f12f2ceb716c83d8b
Anup30giri <70460866+Anup30giri@users.noreply.github.com>
2023-01-22 17:54:23 +0545

    profile

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { useContext } from "react";
+import { ContextUser } from "../Context/UserContext";
 
 const Profile = () => {
+  const user = useContext(ContextUser);
   return (
-    <div className="profile-page">
-      <section className="my-profile">
-        <h3 className="text-center text-primary fw-bold">My Profile</h3>
-      </section>
-      <section className="my-appointments">
-        {" "}
-        <h3 className="text-center text-primary fw-bold">My Profile</h3>
+    <div className="profile-page d-flex p-3">
+      <div
+        style={{ flex: "2" }}
+        className="personal-info d-flex justify-content-center flex-column"
+      >
+        <h3 style={{ color: "orange" }} className="py-3  fw-bold">
+          Personal Information
+        </h3>
+        <div className="personal-info-content">
+          <div className="personal-info-content-item">
+            <p className="text-primary fw-bold">Name</p>
+            <p className="text-secondary">{user && user.name}</p>
+          </div>
+          <div className="personal-info-content-item">
+            <p className="text-primary fw-bold">Email</p>
+            <p className="text-secondary">{user && user.email}</p>
+          </div>
+          <div className="personal-info-content-item">
+            <p className="text-primary fw-bold">Account Created At</p>
+            <p className="text-secondary">
+              {user && user.createdAt.substring(0, 10)}
+            </p>
+          </div>
+        </div>
+      </div>
+      <section className="my-appointments" style={{ flex: "3" }}>
+        <h3 style={{ color: "orange" }} className=" py-3 text-center  fw-bold">
+          My Appointments
+        </h3>
       </section>
     </div>
   );
